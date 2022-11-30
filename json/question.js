cf2947334af894fd9b027474a61c50c2a0b5cd4d
unknown <UV@DGTI-COA807.coatza.intra.uv.mx>
2022-11-30 10:03:14 -0600

    first commit

---
+++ json/question.js
@@ -0,0 +1,103 @@
+
+data = [
+    {
+        question:"¿En qué idioma está la serie de Ladybug?",
+        answers:{
+            a:"Ingles",
+            b:"Fránces",
+            c:"Portugues",
+            d:"Chino"
+        },
+        correctAnswer:"b"
+    },
+    {
+        question: "¿Cuándo y porqué fue el primer beso de boca de CatNoir y LadyBug?",
+        answers:{
+            a:"Oblivio, porque perdieron la memoria",
+            b:"En la cena que preparó CatNoir, porque se gustaban",
+            c:"Cuando CatNoir fue hechizado y LadyBug lo besó para deshacer el hechizo",
+            d:"Nunca se besaron"
+        },
+        correctAnswer:"c"
+    },
+    {
+        question: "¿Cuál es la traducción del primer apellido de Marinette?",
+        answers:{
+            a:"Pan",
+            b:"Dulzura",
+            c:"Hamster",
+            d:"Caramelo"
+        },
+        correctAnswer:"a"
+    },
+    {
+        question: "¿En qué parte de la casa se encuentra la habitación de Marinette?",
+        answers:{
+            a:"Living",
+            b:"Panadería",
+            c:"Sótano",
+            d:"Ático"
+        },
+        correctAnswer:"d"
+    },
+    {
+        question: "¿Cómo se llama el viejo guardián de los Miraculous?",
+        answers:{
+            a:"Wang Fu",
+            b:"Yan Fu",
+            c:"Long Fu",
+            d:"Mong Fu"
+        },
+        correctAnswer:"a"
+    },
+    {
+        question: "¿Cómo se llaman los padres de Marinette?",
+        answers:{
+            a:"Tom y Sabine",
+            b:"Dom y Sabine",
+            c:"Thom y Summer",
+            d:"Tom y Sabrina"
+        },
+        correctAnswer:"a"
+    },
+    {
+        question: "Luka es:",
+        answers:{
+            a:"Primo de Juleka",
+            b:"Hermanastro de Juleka",
+            c:"Amigo de Juleka",
+            d:"Hermano de Juleka"
+        },
+        correctAnswer:"d"
+    },
+    {
+        question: "Cuál de estos no es un kwami?",
+        answers:{
+            a:"Lobo",
+            b:"Caballo",
+            c:"Pavo Real",
+            d:"Tigre"
+        },
+        correctAnswer:"a"
+    },
+    {
+        question: "¿Quién fue akumatizado como Pixeleador?",
+        answers:{
+            a:"El padre de Sabrina",
+            b:"Un fan de Jagged Stone",
+            c:"El admirador de Adrien",
+            d:"Un hipnotizador"
+        },
+        correctAnswer:"c"
+    },
+    {
+        question: "¿En qué capitulo Marinette casi besa a Adrien pensando que era un estatua?",
+        answers:{
+            a:"Clima Tempestuoso",
+            b:"La titiritera",
+            c:"La titiritera(parte 2)",
+            d:"Prime Queen"
+        },
+        correctAnswer:"c"
+    }
+];
